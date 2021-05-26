bcf34e07e9dd0c2d930360c6eb219b25cbcd6249
zachkohl <zach.kohl@gmail.com>
2021-05-25 19:39:59 -0700

    added minimal security

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask import request
+from dotenv import load_dotenv
+load_dotenv()
+import os
+SECRET_KEY = os.getenv("pythonapi")
+
+
 app = Flask(__name__)
+
+global tempTest
+tempTest = 'Hello fellow PACers!'
 
 @app.route('/')
 def index():
-    return 'Hello fellow PACers!'
+    global tempTest
+    return tempTest
+
+
+
+@app.route('/create',methods=["POST"])
+def create():
+    params = request.json
+    if SECRET_KEY == params["key"]:
+        global tempTest
+        tempTest  = params["name"]
+    return 'complete'
